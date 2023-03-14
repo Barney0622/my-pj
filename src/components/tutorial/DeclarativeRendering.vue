3b6ddf6d3b8b2e3df0716ce0c9fc6614d5c2d17c
barney <barney.shen@linkage-retail.com>
2023-03-14 10:45:42 +0800

    DeclarativeRendering

---
+++ src/components/tutorial/DeclarativeRendering.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "DeclarativeRendering",
+  setup() {
+    const message = ref("HelloWorld!");
+
+    return {
+      message,
+    };
+  },
+});
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <DeclarativeRendering></DeclarativeRendering>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import DeclarativeRendering from "@/components/tutorial/DeclarativeRendering.vue";
+
+export default defineComponent({
+  name: "AboutView",
+  components: {
+    DeclarativeRendering,
+  },
+});
+</script>
