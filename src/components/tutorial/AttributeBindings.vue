9cb1abe5eb65c68ba445c4f80a7e5f65bc6c809a
barney <barney.shen@linkage-retail.com>
2023-03-14 12:08:00 +0800

    AttributeBindings

---
+++ src/components/tutorial/AttributeBindings.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 :class="titleClass">Make me red</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "AttributeBindings",
+  setup() {
+    const titleClass = ref("title");
+    return {
+      titleClass,
+    };
+  },
+});
+</script>
+
+<style>
+.title {
+  color: red;
+}
+</style>
