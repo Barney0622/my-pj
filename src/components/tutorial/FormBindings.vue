2b32e5ec7a4f08b91d79aba186be1540a40c1147
barney <barney.shen@linkage-retail.com>
2023-03-14 13:49:57 +0800

    FormBindings

---
+++ src/components/tutorial/FormBindings.vue
@@ -0,0 +1,23 @@
+<template>
+  <input v-model="text" placeholder="Type here" />
+  <p>{{ text }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "FormBindings",
+  setup() {
+    const text = ref("");
+    const onInput = (event: InputEvent) => {
+      text.value = (event.target as HTMLInputElement).value;
+    };
+
+    return {
+      onInput,
+      text,
+    };
+  },
+});
+</script>
