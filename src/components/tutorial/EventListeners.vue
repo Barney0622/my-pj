097cd36faf9e6729a1f515fead0cb27c9943f18a
barney <barney.shen@linkage-retail.com>
2023-03-14 12:18:05 +0800

    EventListeners

---
+++ src/components/tutorial/EventListeners.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="increment">count is: {{ count }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "EventListeners",
+  setup() {
+    const count = ref(0);
+    const increment = () => {
+      count.value++;
+    };
+    return {
+      count,
+      increment,
+    };
+  },
+});
+</script>
