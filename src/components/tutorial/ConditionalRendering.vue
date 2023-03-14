a91a959a43364c01b632a00c12c3c423b987648f
barney <barney.shen@linkage-retail.com>
2023-03-14 14:23:48 +0800

    ConditionalRendering

---
+++ src/components/tutorial/ConditionalRendering.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="toggle">toggle</button>
+  <h1 v-if="awesome.value">This is awesome!</h1>
+  <h1 v-else>Not awesome!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+
+export default defineComponent({
+  name: "ConditionalRendering",
+  setup() {
+    const awesome = reactive({
+      value: false,
+    });
+    const toggle = () => {
+      awesome.value = !awesome.value;
+    };
+    return {
+      toggle,
+      awesome,
+    };
+  },
+});
+</script>
