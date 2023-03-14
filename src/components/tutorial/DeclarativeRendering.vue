3ae4238990c123817896964f1694bbba5e319e87
barney <barney.shen@linkage-retail.com>
2023-03-14 11:07:22 +0800

    DeclarativeRendering

--- src/components/tutorial/DeclarativeRendering.vue
+++ src/components/tutorial/DeclarativeRendering.vue
@@ -1,17 +1,21 @@
 <template>
   <h1>{{ message }}</h1>
+  <h1>count: {{ counter.count }}</h1>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, reactive } from "vue";
 
 export default defineComponent({
   name: "DeclarativeRendering",
   setup() {
     const message = ref("HelloWorld!");
-
+    const counter = reactive({
+      count: 0,
+    });
     return {
       message,
+      counter,
     };
   },
 });
